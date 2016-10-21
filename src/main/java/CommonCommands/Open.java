8b0c9a9a97d98e3ccbe56e19b9b029a3f686424e
Kalkidan <2008kalu@gmail.com>
2016-10-21 18:47:49 -0400

    3rd commit

--- src/main/java/CommonCommands/Open.java
+++ src/main/java/CommonCommands/Open.java
@@ -2,8 +2,9 @@ package CommonCommands;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 /**
  * Created by Kalkidan on 10/20/2016.
@@ -12,10 +13,11 @@ public class Open {
 
   @Test
     public void testOpen(){
-      WebDriver driver=new FirefoxDriver();
-      driver.get("https://www.google.com");
-
-      driver.close();
+    File file = new File("C:/Users/Kalkidan/IdeaProjects/RampUp/src/Selenium_Drivers/chromedriver.exe");
+    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://www.huffingtonpost.com");
+    driver.close();
   }
 
 }
