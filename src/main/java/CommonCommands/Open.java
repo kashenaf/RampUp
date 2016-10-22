99531b7788c8a27cd1ce4925517e137befb5b63a
Kalkidan <2008kalu@gmail.com>
2016-10-22 14:00:31 -0400

    4th commit

--- src/main/java/CommonCommands/Open.java
+++ src/main/java/CommonCommands/Open.java
@@ -1,5 +1,6 @@
 package CommonCommands;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -13,11 +14,28 @@ public class Open {
 
   @Test
     public void testOpen(){
-    File file = new File("C:/Users/Kalkidan/IdeaProjects/RampUp/src/Selenium_Drivers/chromedriver.exe");
-    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-    WebDriver driver = new ChromeDriver();
-    driver.get("https://www.huffingtonpost.com");
-    driver.close();
+      File file = new File("C:/Users/Kalkidan/IdeaProjects/RampUp/src/Selenium_Drivers/chromedriver.exe");
+      System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+      WebDriver driver = new ChromeDriver();
+      driver.get("https://www.huffingtonpost.com");
+      driver.close();
   }
-
+  @Test
+    public void testOpen1(){
+      File file= new File("C:/Users/Kalkidan/IdeaProjects/RampUp/src/Selenium_Drivers/chromedriver.exe");
+      System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+      WebDriver driver = new ChromeDriver();
+      driver.get("https://www.google.com");
+      Base commands = new Base();
+      driver.close();
+  }
+  @Test
+    public void testOpen2(){
+      File file = new File("C:/Users/Kalkidan/IdeaProjects/RampUp/src/Selenium_Drivers/chromedriver.exe");
+      System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+      WebDriver driver = new ChromeDriver();
+      driver.get("https://www.cnn.com");
+      Base commands = new Base();
+      driver.close();
+  }
 }
